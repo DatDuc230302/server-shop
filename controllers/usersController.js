3fab833b7edaa60da9ec2c1aa020430bc6064bb6
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-06-09 22:44:27 +0700

    D

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,3 +17,17 @@ export const findIdUsers = async (req, res, next) => {
     const data = await users.find({ id: body.id });
     res.json(data);
 };
+
+export const addCarts = async (req, res, next) => {
+    const body = req.body;
+    const data = users
+        .findOneAndUpdate({ id: body.id }, { $push: { carts: body.item } }, { new: true })
+        .exec()
+        .then((updatedUser) => {
+            updatedUser;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    res.json('OK');
+};
--- routers/users.js
+++ routers/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addUsers, findIdUsers, getUsers } from '../controllers/usersController.js';
+import { addCarts, addUsers, findIdUsers, getUsers } from '../controllers/usersController.js';
 
 const usersRouter = express.Router();
 
 usersRouter.get('/users/get', getUsers);
 usersRouter.post('/users/add', addUsers);
 usersRouter.post('/users/findId', findIdUsers);
+usersRouter.post('/users/addCarts', addCarts);
 
 export default usersRouter;
