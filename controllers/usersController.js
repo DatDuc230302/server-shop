8076abdb988f57e846d50a9f6e73c2d72b9fb160
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-06-13 15:49:38 +0700

    D

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,5 +29,59 @@ export const addCarts = async (req, res, next) => {
         .catch((err) => {
             console.error(err);
         });
-    res.json('OK');
+    res.json();
+};
+
+export const updateCarts = async (req, res, next) => {
+    const body = req.body;
+    const data = await users.findOne({ id: body.id }, { carts: 1 });
+    const quantity = body.newArr.length;
+    const lenArr = data.carts.filter((it) => it === body.item).length;
+    let count = 0;
+    const filteredArr = data.carts.filter((obj) => {
+        if (obj === body.item) {
+            count++;
+            return count <= quantity;
+        }
+        return true;
+    });
+
+    if (lenArr > quantity) {
+        await users
+            .findOneAndUpdate({ id: body.id }, { carts: filteredArr }, { new: true })
+            .then((updatedDocument) => {
+                updatedDocument;
+            })
+            .catch((error) => {
+                error;
+            });
+        res.json();
+    } else {
+        for (let i = 0; i < quantity - lenArr; i++) {
+            data.carts.push(body.item);
+        }
+        await users
+            .findOneAndUpdate({ id: body.id }, { carts: data.carts }, { new: true })
+            .then((updatedDocument) => {
+                updatedDocument;
+            })
+            .catch((error) => {
+                error;
+            });
+        res.json();
+    }
+};
+
+export const deleteCarts = async (req, res, next) => {
+    const body = req.body;
+
+    await users
+        .updateMany({}, { $pull: { carts: body.item } })
+        .then((result) => {
+            result;
+        })
+        .catch((error) => {
+            error;
+        });
+    res.json();
 };
--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addCarts, addUsers, findIdUsers, getUsers } from '../controllers/usersController.js';
+import { addCarts, addUsers, deleteCarts, findIdUsers, getUsers, updateCarts } from '../controllers/usersController.js';
 
 const usersRouter = express.Router();
 
@@ -7,5 +7,7 @@ usersRouter.get('/users/get', getUsers);
 usersRouter.post('/users/add', addUsers);
 usersRouter.post('/users/findId', findIdUsers);
 usersRouter.post('/users/addCarts', addCarts);
+usersRouter.post('/users/updateCarts', updateCarts);
+usersRouter.post('/users/deleteCarts', deleteCarts);
 
 export default usersRouter;
